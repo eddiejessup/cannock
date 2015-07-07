65da1b49d86595fe0043256e00c6c596c7c38f33
Elliot Marsden <elliot.marsden@gmail.com>
2015-07-07 18:13:30 +0100

    Add script to plot stability over parameter sweep.

---
+++ plot_param_sweep.py
@@ -0,0 +1,50 @@
+from __future__ import print_function, division
+import matplotlib.pyplot as plt
+import numpy as np
+from ciabatta import ejm_rcparams
+from ciabatta.ejm_rcparams import set2
+
+save_flag = True
+
+use_latex = save_flag
+use_pgf = True
+
+ejm_rcparams.set_pretty_plots(use_latex, use_pgf)
+
+fig = plt.figure(figsize=(12, 12 * ejm_rcparams.golden_ratio))
+ax = fig.add_subplot(111)
+ejm_rcparams.prettify_axes(ax)
+
+D_rho, mu, m, v = np.loadtxt('data.txt', unpack=True)
+
+C = 0.1
+
+ax = fig.add_subplot(111)
+unstable = v > 1e-7
+c = np.zeros_like(v)
+c[unstable] = 1.0
+D_rhos = np.linspace(0.1 * D_rho.min(), 10.0 * D_rho.max(), 1000)
+stable_mus = D_rhos
+ax.scatter(D_rho[np.logical_not(unstable)], mu[np.logical_not(unstable)],
+           c=set2[1], marker='o', label='Stable', s=40)
+ax.scatter(D_rho[unstable], mu[unstable],
+           c=set2[0], marker='s', label='Unstable', s=40)
+ax.plot(D_rhos, stable_mus,
+        c=set2[2], label=r'$\mu = D_\rho$', lw=5)
+
+handles, labels = ax.get_legend_handles_labels()
+handles = handles[1], handles[2], handles[0]
+labels = labels[1], labels[2], labels[0]
+ax.legend(handles, labels, loc='lower right', fontsize=26, frameon=True)
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel(r'$\tilde{D}_\rho$', fontsize=35)
+ax.set_ylabel(r'$\tilde{\mu}$', fontsize=35)
+ax.tick_params(axis='both', labelsize=26, pad=10.0)
+ax.set_xlim(0.9 * D_rho.min(), 1.1 * D_rho.max())
+ax.set_ylim(0.9 * mu.min(), 1.1 * mu.max())
+
+if save_flag:
+    plt.savefig('sweep_D_mu.pdf', bbox_inches='tight')
+else:
+    plt.show()
